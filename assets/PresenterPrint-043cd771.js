2969a5bd50f77fbcf194d1ecc595e63887bfdde1
sourenaKhanzadeh <50275269+sourenaKhanzadeh@users.noreply.github.com>
2023-03-14 03:09:36 +0000

    Deploying to gh-pages from @ sourenaKhanzadeh/DeepFlow@5be158466a19b2e8daeb7315a8a84849839f1e0d 🚀

--- assets/PresenterPrint-043cd771.js
+++ assets/PresenterPrint-043cd771.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as k,m as w,p as N,q as P,_ as D}from"./index-ceaee525.js";import{N as S}from"./NoteDisplay-863400d2.js";const V={class:"m-4"},j={class:"mb-10"},F={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},T={class:"text-lg"},B={class:"font-bold flex gap-2"},C={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as k,m as w,p as N,q as P,_ as D}from"./index-1ff4b8b7.js";import{N as S}from"./NoteDisplay-eab306cd.js";const V={class:"m-4"},j={class:"mb-10"},F={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},T={class:"text-lg"},B={class:"font-bold flex gap-2"},C={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
