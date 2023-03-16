d8d4c0fff0d78c207b6cbfe307120cd908f3241b
sourenaKhanzadeh <50275269+sourenaKhanzadeh@users.noreply.github.com>
2023-03-16 16:40:38 +0000

    Deploying to gh-pages from @ sourenaKhanzadeh/DeepFlow@95f1096a4c3f69d382f5a28c81615d5937816c91 🚀

---
+++ assets/PresenterPrint-a88c93d5.js
@@ -0,0 +1,17 @@
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as l,k as b,l as k,m as w,p as N,q as P,_ as D}from"./index-ea0dd28d.js";import{N as S}from"./NoteDisplay-44accb26.js";const V={class:"m-4"},j={class:"mb-10"},F={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},T={class:"text-lg"},B={class:"font-bold flex gap-2"},C={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),z={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),h(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),u({title:`Notes - ${m.title}`});const i=f(()=>y.slice(0,-1).map(o=>{var r;return(r=o.meta)==null?void 0:r.slide}).filter(o=>o!==void 0&&o.noteHTML!==""));return(o,r)=>(l(),n("div",{id:"page-root",style:x(a(P))},[t("div",V,[t("div",j,[t("h1",F,s(a(m).title),1),t("div",L,s(new Date().toLocaleString()),1)]),(l(!0),n(g,null,v(a(i),(e,c)=>(l(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",T,[t("div",B,[t("div",C,s(e==null?void 0:e.no)+"/"+s(a(b)),1),k(" "+s(e==null?void 0:e.title)+" ",1),H])]),w(S,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<a(i).length-1?(l(),n("hr",z)):N("v-if",!0)]))),128))])],4))}}),R=D(M,[["__file","/home/runner/work/DeepFlow/DeepFlow/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{R as default};
